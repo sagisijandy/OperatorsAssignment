4f40345029f3e3082c49c7bca01f8537ce0db80d
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2023-05-11 13:25:35 -0700

    Add project files.

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2) 
+        {
+            return emp1.Id == emp2.Id;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2) 
+        {
+            return !(emp1 == emp2);
+        }
+
+          
+
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1=new Employee () { Id=1, FirstName="John", LastName="Doe"};
+            Employee emp2=new Employee () { Id=2, FirstName="Jane", LastName="Doe" };
+
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("Employees are equal.");
+            }
+            else
+            {
+                Console.WriteLine("Employees are not equal");
+            }
+            
+        }
+    }
+}
